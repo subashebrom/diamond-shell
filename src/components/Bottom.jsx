2f6facc566f8cf873a7fb687643c47e7ffa39f13
subashebrom <subashembrom99@gmail.com>
2025-07-22 19:28:35 +0600

    footer and home page game add

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,7 +1,13 @@
+import { faDiscord, faFacebook, faInstagram, faSnapchat, faWhatsapp, faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router";
+
 const Bottom = () => {
     return (
-        <div className="w-full h-auto text-white bg-mainbg">
-            <footer className="relative w-[80%] mx-auto border-r border-r-gray-700 border-l border-l-gray-700 px-8 border-t border-t-gray-700">
+        <div className="w-full h-auto text-white bg-mainbg font-Ubuntu">
+            <footer className="relative w-[80%] mx-auto border-r border-r-gray-700 border-l border-l-gray-700 px-8 border-t border-t-gray-700 py-12">
                 {/* === DIAMOND === */}
                 <div className="absolute -top-10 -right-5 size-10 z-80">
                     <img src="/image/diamond.png" alt="" />
@@ -9,7 +15,78 @@ const Bottom = () => {
                 <div className="absolute -top-10 -left-5 size-10 z-80">
                     <img src="/image/diamond.png" alt="" />
                 </div>
-                <h1>Subas</h1>
+                <section className="lg:grid lg:grid-cols-4 items-start">
+                    {/* === Logo Section === */}
+                    <div>
+                        <h1 className="lg:text-xl">Logo image</h1>
+                    </div>
+                    {/* === About Section === */}
+                    <div className="grid gap-0">
+                        <h1 className="lg:text-xl">Contact Us</h1>
+                        <div className="grid mt-1">
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                <span className="mr-1 lg:text-xl"> <FontAwesomeIcon icon={faDiscord}/> </span>
+                                Join Our Discord Server
+                            </NavLink>
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                <span className="mr-1 lg:text-xl"> <FontAwesomeIcon icon={faWhatsapp}/> </span>
+                                Chat with Us!
+                            </NavLink>
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                <span className="mr-1 lg:text-xl"> <FontAwesomeIcon icon={faPhone}/> </span>
+                                +880 1315291293
+                            </NavLink>
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                <span className="mr-1 lg:text-xl"> <FontAwesomeIcon icon={faEnvelope}/> </span>
+                                subashebrom@gmail.com
+                            </NavLink>
+                        </div>
+                    </div>
+                    {/* === Service Section === */}
+                    <div className="lg:grid">
+                        <h1 className="lg:text-xl">Security</h1>
+                        <div className="grid mt-1">
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                Privecy Policy
+                            </NavLink>
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                                Refund Policy
+                            </NavLink>
+                            <NavLink to='' className="lg:text-lg hover:text-hoverbdr transition-all duration-200 delay-75 ease-in-out">
+                               Terms and Conditions
+                            </NavLink>
+                        </div>
+                    </div>
+                    {/* === Social Section === */}
+                    <div className="w-full lg:flex lg:flex-col gap-2">
+                        <h1 className="lg:text-xl">Social</h1>
+                        <div className="flex items-center justify-between px-2 py-1 gap-2 lg:text-xl border border-gray-700 rounded-full hover:bg-blue-600 hover:scale-105 transition-all duration-400 dealy-75 ease-in-out">
+                            <span className="lg:text-2xl"><FontAwesomeIcon icon={faFacebook}/></span>
+                            <h2 className="lg:font-semibold lg:text-xl">Facebook</h2>
+                            <span></span>
+                        </div>
+                        <div className="flex items-center justify-between px-2 py-1 gap-2 lg:text-xl border border-gray-700 rounded-full hover:bg-red-600 hover:scale-105 transition-all duration-400 dealy-75 ease-in-out">
+                            <span className="lg:text-2xl"><FontAwesomeIcon icon={faYoutube}/></span>
+                            <h2 className="lg:font-semibold lg:text-xl">Youtube</h2>
+                            <span></span>
+                        </div>
+                        <div className="flex items-center justify-between px-2 py-1 gap-2 lg:text-xl border border-gray-700 rounded-full hover:bg-hoverbdr hover:scale-105 transition-all duration-400 dealy-75 ease-in-out">
+                            <span className="lg:text-2xl"><FontAwesomeIcon icon={faXTwitter}/></span>
+                            <h2 className="lg:font-semibold lg:text-xl">Twitter</h2>
+                            <span></span>
+                        </div>
+                        <div className="flex items-center justify-between px-2 py-1 gap-2 lg:text-xl border border-gray-700 rounded-full hover:bg-gradient-to-r from-fuchsia-500 to-purple-600 hover:scale-105 transition-all duration-400 dealy-75 ease-in-out">
+                            <span className="lg:text-2xl"><FontAwesomeIcon icon={faInstagram}/></span>
+                            <h2 className="lg:font-semibold lg:text-xl">Instagram</h2>
+                            <span></span>
+                        </div>
+                        <div className="flex items-center justify-between px-2 py-1 gap-2 lg:text-xl border border-gray-700 rounded-full hover:bg-yellow-400 hover:scale-105 transition-all duration-400 dealy-75 ease-in-out">
+                            <span className="lg:text-2xl"><FontAwesomeIcon icon={faSnapchat}/></span>
+                            <h2 className="lg:font-semibold lg:text-xl">Snapchat</h2>
+                            <span></span>
+                        </div>
+                    </div>
+                </section>
             </footer>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,82 +12,184 @@ const Home = () => {
                 {/* === CARDS SECTION === */}
                 <div className="w-full px-4 py-4 grid grid-cols-4 gap-6 text-gray-900">
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
                         <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">FREE FIRE</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
                         <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/pubg.png" alt="" />
-                        <h2 className="lg:text-lg py-2">PUBG</h2>
+                        <h2 className="lg:text-lg py-2">PUBG MOBILE</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
                         <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/call-of-duty.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">CALL OF DUTY</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
                         <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/valorant.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">VALORANT</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/apex-legend.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">APEX LEGEND</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/warezone.webp" alt="" />
                         <h2 className="lg:text-lg py-2">WARZONE</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/blood-strike.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">BLOOD STRIKE</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/doom-eternal.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">DOOM ETERNAL</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/elden.webp" alt="" />
                         <h2 className="lg:text-lg py-2">ELDEN RING</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/cyberpunk.jpg" alt="" />
                         <h2 className="lg:text-lg py-2">CYBER PUNK 2077</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/the-witcher.webp" alt="" />
                         <h2 className="lg:text-lg py-2">THE WITCHER 3:WILD HUNT</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
-                        <h2 className="lg:text-lg py-2">STARFIELD</h2>
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">BALDUR'S GATE 3</h2>
                     </NavLink>
                     <NavLink to=''
-                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:-translate-y-2 hover:rotate-y-22 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
                     >
-                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/free-fire.jpg" alt="" />
-                        <h2 className="lg:text-lg py-2">BALDUR'S GATE 3</h2>
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">FORTNITE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">CLASS OF CLANCE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">DELTA FORCE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">FAR LIGHT 84</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">GANSHIN IMPACT</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">LEAGUE OF LEGENDS</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">MOBILE LEGENDS: BANG BANG</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">SAUSAGE MAN</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">SOLO LEVELING: ARISE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">ASPHALT LEGEND</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">CALL OF DUTY MOBILE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">ROBLOX</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">BRAWL STARS</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">SMASH LEGENDS</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">DEVIL MAY CRY</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">CLASH ROYALE</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">MORTAL KOMBAT</h2>
+                    </NavLink>
+                    <NavLink to=''
+                    className="w-full text-center bg-white rounded-lg border border-gray-700 hover:text-hoverbdr hover:scale-105 hover:border-hoverbdr transition-all duration-400 dealy-75 hover:w-full ease-in-out"
+                    >
+                        <img className="w-full h-62 rounded-t-lg" src="/src/assets/image/baldurs.jpg" alt="" />
+                        <h2 className="lg:text-lg py-2">BRAWL HALLA</h2>
                     </NavLink>
                 </div>
             </section>
